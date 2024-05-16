887fd1eae5b0f8bb79f3536cd210239f48e75e28
César Van Leuffelen <info@netbrew.be>
2024-05-16 22:53:13 +0200

    decision tool v0.1
    you can now give a score for importants of that evaluation criteria and depending on that the 2 best choices are displayed.

---
+++ app/main.js
@@ -0,0 +1,5 @@
+let brief = {'cat1': 1,'cat2': 3,'cat3': 2,'cat4': 2,'cat5': 3,'cat6': 1,}
+
+let mail = {'cat1': 2,'cat2': 2,'cat3': 3,'cat4': 1,'cat5': 5,'cat6': 6,}
+
+let facebook = {'cat1': 3,'cat2': 1,'cat3': 1,'cat4': 3,'cat5': 2,'cat6': 2,}
